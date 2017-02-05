0816d89a967a4f211f48b4ee36394e65ecfdcf62
Changhui Xu <changhuixu@hotmail.com>
2017-02-05 08:44:17 -0600

    ensure min packet value

--- RedPacket/RedPackets.cs
+++ RedPacket/RedPackets.cs
@@ -9,51 +9,57 @@ namespace RedPacket
         private readonly Random _rand;
         private readonly decimal _minMoneyForEachPacket;
         private readonly decimal _totalMoney;
-        private readonly int _totalNumOfPacket;
+        private readonly int _numOfPackets;
         public List<decimal> Packets { get; }
-        public RedPackets(decimal totalMoney, int totalNumOfPacket, decimal minMoneyForEachPacket = 0.01m)
+        public RedPackets(decimal totalMoney, int numOfPackets, decimal minMoneyForEachPacket = 0.01m)
         {
             _rand = new Random();
             _totalMoney = totalMoney;
-            _totalNumOfPacket = totalNumOfPacket;
+            _numOfPackets = numOfPackets;
             _minMoneyForEachPacket = minMoneyForEachPacket;
 
             EnsureEachPacketHasMinMoney();
-            Packets = RandomizeWithOnlyOneLuckestDraw();
+            Packets = Randomize();
         }
 
         private void EnsureEachPacketHasMinMoney()
         {
-            if (_totalNumOfPacket * _minMoneyForEachPacket > _totalMoney)
+            if (_numOfPackets * _minMoneyForEachPacket > _totalMoney)
             {
-                throw new ArgumentException($"Total money is not enough for {_totalNumOfPacket} packets.");
+                throw new ArgumentException($"Total money is not enough for {_numOfPackets} packets.");
             }
         }
 
+        private List<decimal> GetRandomNumberList()
+        {
+            var packets = new List<decimal>();
+            for (var i = 0; i < _numOfPackets; i++)
+            {
+                var randomNumber = _rand.Next(0, (int)_totalMoney / _numOfPackets * 100);
+                packets.Add(randomNumber);
+            }
+            return packets;
+        }
         private List<decimal> RandomizeWithOnlyOneLuckestDraw()
         {
-            var packets = Randomize();
+            var packets = GetRandomNumberList();
             while (true)
             {
                 var max = packets.Max();
                 if (packets.Count(x => x == max) == 1) break;
-                packets = Randomize();
+                packets = GetRandomNumberList();
             }
             return packets;
         }
 
         private List<decimal> Randomize()
         {
-            var packets = new List<decimal>();
-            for (var i = 0; i < _totalNumOfPacket; i++)
-            {
-                var randomNumber = _rand.Next(1, (int)_totalMoney * 100);
-                packets.Add(randomNumber);
-            }
-            var ratio = packets.Sum(x => x) / _totalMoney;
-            packets = packets.Select(x => decimal.Round(x / ratio, 2)).ToList();
+            var remain = _totalMoney - _minMoneyForEachPacket * _numOfPackets;
+            var packets = RandomizeWithOnlyOneLuckestDraw();
+            var ratio = packets.Sum(x => x) / remain;
+            packets = packets.Select(x => decimal.Round(x / ratio, 2) + _minMoneyForEachPacket).ToList();
             var difference = packets.Sum(x => x) - _totalMoney;
-            packets[_totalNumOfPacket - 1] -= difference;
+            if (Math.Abs(difference) >= 0.01m) packets[_rand.Next(0, _numOfPackets)] -= difference;
             return packets;
         }
     }
